ea969a0e34aac8911742ff3a46b583ea501cb594
Badar Usman <baderusman23@gmail.com>
2024-03-03 10:51:27 +0400

    feat(postcss): added postcss and autoprefixer config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: process.env.NODE_ENV !== "production" && true },
-  css: [
-    "@/assets/css/main.css"
-  ],
-})
+  css: ["@/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: { config: "./tailwind.config.ts" },
+      autoprefixer: {},
+      ...(process.env.NODE_ENVIRONMENT === "production" ? { cssnano: {} } : {}),
+    },
+  },
+});
