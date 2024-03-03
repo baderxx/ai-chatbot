5ac3da2e7c9f9ebd4f6c304b088cdbb8a85a63b2
Badar Usman <baderusman23@gmail.com>
2024-03-03 10:46:38 +0400

    feat(es-lint-prettier): added eslint and prettier

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <button
+      class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+    >
+      Button
+    </button>
   </div>
 </template>
